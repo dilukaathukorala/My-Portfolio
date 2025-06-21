ac995ed6ba0e888fa2ee4cdd326a1cc0f7aed978
Diluka Athukorala <dilukaathukorala@gmail.com>
2025-06-21 17:29:24 +0530

    Update navigation bar

--- my-portfolio/components/Navbar.js
+++ my-portfolio/components/Navbar.js
@@ -1,11 +1,11 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-900 text-white p-4 shadow-md">
-      <div className="max-w-5xl mx-auto flex justify-between items-center">
+    <nav className="bg-blue-800 text-white px-6 py-4 shadow-md">
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
         <h1 className="text-xl font-bold">Leo</h1>
-        <div className="space-x-4">
+        <div className="space-x-6">
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
           <Link href="/projects">Projects</Link>
