5f9ded970dab4f14c2d3d5c5f4b88862da5811fb
Diluka Athukorala <dilukaathukorala@gmail.com>
2025-06-30 00:17:23 +0530

    Update page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Hero from '../components/Hero';
-import Navbar from '../components/Navbar';
+
 
 export default function Home(){
   return(
     <>
       <Hero/>
-      <Navbar/>
+      
     </>
   );
 }
