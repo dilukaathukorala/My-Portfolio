7f69f00471a91a2ec6885f48f392883582298fdd
Diluka Athukorala <dilukaathukorala@gmail.com>
2025-06-29 17:27:19 +0530

    Update index.js
    import hero intoto index.js

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,20 +1,20 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import styles from '../styles/Home.module.css';
+import { Geist, Geist_Mono } from 'next/font/google';
+import Hero from '../components/Hero';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export default function Home() {
   return (
-    <section className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
-      <img src="/images/Portfolio.jpg" alt="Portfolio Logo" className={styles.logo} />
-    </section>
+    <div className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
+      <Hero />
+    </div>
   );
 }
