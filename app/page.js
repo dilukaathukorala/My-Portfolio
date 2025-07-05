b9af88c1ed4fe52e43a7525c51549a48bd2abd05
Diluka Athukorala <dilukaathukorala@gmail.com>
2025-07-05 23:44:36 +0530

    Update app
    Coded for the navigation animation

--- app/page.js
+++ app/page.js
@@ -1,10 +1,20 @@
+'use client';
+
+import { useState } from 'react';
 import Hero from '../components/Hero';
+import About from '../components/About';
 
+export default function Home() {
+  const [activeSection, setActiveSection] = useState("hero");
 
-export default function Home(){
-  return(
+  return (
     <>
-      <Hero/>
+      {activeSection === "hero" && (
+        <Hero setActiveSection={setActiveSection} />
+      )}
+      {activeSection === "about" && (
+        <About />
+      )}
     </>
   );
 }
