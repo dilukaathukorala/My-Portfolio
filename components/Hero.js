aafd654093994aff8967d3cacba7da6189c586d4
Diluka Athukorala <dilukaathukorala@gmail.com>
2025-07-06 00:32:30 +0530

    Update hero
    added shake effect to see my projects arrow

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,8 @@
 
 import React, { useState, useEffect } from "react";
 import "../styles/Hero.css";
-import Header from "./Header"; // ✅ Modular header
+import Header from "./Header";
+import SocialMedia from "./SocialMedia";
 import { motion } from "framer-motion";
 
 export default function Hero({ setActiveSection }) {
@@ -101,8 +102,9 @@ export default function Hero({ setActiveSection }) {
               </button>
             )}
             <button className="btn secondary" onClick={() => handleNavClick("about")}>
-              See my work →
+             See my work <span className="arrow">➜</span>
             </button>
+
           </div>
         </motion.div>
 
@@ -138,67 +140,8 @@ export default function Hero({ setActiveSection }) {
         </motion.div>
       </div>
 
-      {/* ✅ Fixed Social Icons */}
-      <ul className="example-2 fixed-icons">
-        <li className="icon-content">
-          <a href="https://www.linkedin.com/in/diluka-athukorala-703247214/" aria-label="LinkedIn" data-social="linkedin">
-            <div className="filled"></div>
-            <svg className="icon-svg" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 16 16">
-              <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 
-              1.146v13.708c0 .633-.526 1.146-1.175 
-              1.146H1.175C.526 16 0 15.487 0 
-              14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 
-              0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 
-              3.226 2.4 3.934c0 .694.521 1.248 1.327 
-              1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 
-              1.232-.878.869 0 1.216.662 1.216 
-              1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 
-              0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 
-              0 7.225 0 7.225z" />
-            </svg>
-          </a>
-          <div className="tooltip">LinkedIn</div>
-        </li>
-
-        <li className="icon-content">
-          <a href="https://github.com/dilukaathukorala" aria-label="GitHub" data-social="github">
-            <div className="filled"></div>
-            <svg className="icon-svg" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 16 16">
-              <path d="M8 0C3.58 0 0 3.58 0 8c0 
-              3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 
-              0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 
-              1.08.58 1.23.82.72 1.21 1.87.87 
-              2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 
-              0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 
-              0 0 .67-.21 2.2.82.64-.18 
-              1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 
-              2.2-.82 2.2-.82.44 1.1.16 1.92.08 
-              2.12.51.56.82 1.27.82 2.15 
-              0 3.07-1.87 3.75-3.65 
-              3.95.29.25.54.73.54 
-              1.48 0 1.07-.01 1.93-.01 
-              2.2 0 .21.15.46.55.38A8.01 8.01 
-              0 0 0 16 8c0-4.42-3.58-8-8-8" />
-            </svg>
-          </a>
-          <div className="tooltip">GitHub</div>
-        </li>
-
-        <li className="icon-content">
-          <a href="https://www.instagram.com/dilu_athukorala/" aria-label="Instagram" data-social="instagram">
-            <div className="filled"></div>
-            <svg className="icon-svg" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 512 512">
-              <path d="M349.33,69.33H162.67A93.34,93.34,0,0,0,69.33,162.67v186.66A93.34,93.34,0,0,0,162.67,
-              442.67h186.66A93.34,93.34,0,0,0,442.67,349.33V162.67A93.34,93.34,0,0,0,349.33,69.33ZM464,
-              349.33A114.68,114.68,0,0,1,349.33,464H162.67A114.68,114.68,0,0,1,48,349.33V162.67A114.68,
-              114.68,0,0,1,162.67,48H349.33A114.68,114.68,0,0,1,464,162.67ZM256,144a112,112,0,1,0,112,
-              112A112,112,0,0,0,256,144Zm0,186.67A74.67,74.67,0,1,1,330.67,256,74.77,74.77,0,0,1,
-              256,330.67ZM370.67,144a26.67,26.67,0,1,0,26.66,26.67A26.69,26.69,0,0,0,370.67,144Z" />
-            </svg>
-          </a>
-          <div className="tooltip">Instagram</div>
-        </li>
-      </ul>
+      {/* ✅ Fixed Social Icons (Modularized) */}
+      <SocialMedia />
     </section>
   );
 }
